b54c115448a0703a3069d5f476e4803c3b04381f
Muukii <muukii.app@gmail.com>
2018-12-19 19:04:32 +0900

    :evergreen_tree: Update

--- packages/core/lib/index.d.ts
+++ packages/core/lib/index.d.ts
@@ -1,1 +1,10 @@
-export declare function send(deviceToken: string, payload: string, p8FilePath: string, keyID: string, teamID: string, topic: string, isProduction: boolean): Promise<void>;
+export declare type Descriptor = {
+    deviceToken: string;
+    payload: string;
+    p8FilePath: string;
+    keyID: string;
+    teamID: string;
+    topic: string;
+    isProduction: boolean;
+};
+export declare function send(descriptor: Descriptor): Promise<void>;
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,35 +1,37 @@
-import * as apn from "apn";
+import * as apn from 'apn'
 
-export async function send(
-  deviceToken: string,
-  payload: string,
-  p8FilePath: string,
-  keyID: string,
-  teamID: string,
-  topic: string,
+export type Descriptor = {
+  deviceToken: string
+  payload: string
+  p8FilePath: string
+  keyID: string
+  teamID: string
+  topic: string
   isProduction: boolean
-) {
+}
+
+export async function send(descriptor: Descriptor) {
   const options = {
     token: {
-      key: p8FilePath,
-      keyId: keyID,
-      teamId: teamID
+      key: descriptor.p8FilePath,
+      keyId: descriptor.keyID,
+      teamId: descriptor.teamID,
     },
     // proxy: {
     //   host: "192.168.10.92",
     //   port: 8080
     // }
-    production: isProduction
-  };
+    production: descriptor.isProduction,
+  }
 
-  console.log("Send", deviceToken, "options", options);
+  console.log('Send', descriptor.deviceToken, 'options', options)
 
-  const apnProvider = new apn.Provider(options);
+  const apnProvider = new apn.Provider(options)
 
-  const notification = new apn.Notification();
+  const notification = new apn.Notification()
 
-  notification.rawPayload = payload;
-  notification.topic = topic;
+  notification.rawPayload = descriptor.payload
+  notification.topic = descriptor.topic
 
-  await apnProvider.send(notification, deviceToken);
+  await apnProvider.send(notification, descriptor.deviceToken)
 }
