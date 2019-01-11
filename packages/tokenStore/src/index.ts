166815fc3b3be2fa975f9803bbc7f040e6407919
Kenji Tayama <kenji.tayama@gmail.com>
2019-01-11 14:24:31 +0900

    tokenStore wip

--- packages/tokenStore/src/index.ts
+++ packages/tokenStore/src/index.ts
@@ -2,7 +2,13 @@ import * as express from 'express';
 
 // export type express.Request
 
-// export function tokenStoreMiddleware(request: express.Request, response: express.Response, next) {
+// export const tokenStoreMiddleware = (request: express.Request, response: express.Response, next: express.NextFunction) => {
+
+//     console.log(`### ${request.method} ${request.path}`);
+//     next();
+// }
+
+// export function tokenStoreMiddleware(request: express.Request, response: express.Response, next: express.NextFunction) {
 export function tokenStoreMiddleware(request, response, next) {
 
     console.log(`### ${request.method} ${request.path}`);
