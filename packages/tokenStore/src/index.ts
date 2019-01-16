6d4273caae49b20632e12c210c7f5e6fc34972e7
Kenji Tayama <kenji.tayama@gmail.com>
2019-01-16 11:54:52 +0900

    fixed typescript not compiling argument label

--- packages/tokenStore/src/index.ts
+++ packages/tokenStore/src/index.ts
@@ -9,14 +9,10 @@ import * as express from 'express';
 // }
 
 // export function tokenStoreMiddleware(request: express.Request, response: express.Response, next: express.NextFunction) {
-export function tokenStoreMiddleware(request, response, next) {
+export function tokenStoreMiddleware(request: express.Request, response: express.Response, next: express.NextFunction) {
 
     console.log(`### ${request.method} ${request.path}`);
     next();
-}
-
-export function hoge(a: string) {
-    console.log(a);
 }
 
 // export function saveToken(request, response, next) {
